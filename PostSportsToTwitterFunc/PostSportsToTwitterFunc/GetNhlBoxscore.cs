db279a69de5f4c4b4a479619a9760771d6142e7d
Hans Wustrack <hans2792@gmail.com>
2019-10-13 07:34:02 -0500

    use now instead of today for nhl pacific hours

--- PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNhlBoxscore.cs
+++ PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNhlBoxscore.cs
@@ -9,7 +9,7 @@ namespace PostSportsToTwitterFunc
     {
         private const MySportsFeedsClient.Sport Sport = MySportsFeedsClient.Sport.NHL;
         private const string TeamAbbreviation = "STL";
-        private static readonly DateTime ForDate = DateTime.Today - TimeSpan.FromHours(7); // pacific hours so there's time for the game to be finalized
+        private static readonly DateTime ForDate = DateTime.UtcNow - TimeSpan.FromHours(7); // pacific hours so there's time for the game to be finalized
 
         [FunctionName("GetNhlBoxscore")]
         public static async Task Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log)
