872fdf46f056620bc4e980fbcbd2f5a17b0e422a
Hans Wustrack <hans2792@gmail.com>
2019-11-11 14:11:09 +1300

    give a longer window for game to be finalized

--- PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNbaBoxscore.cs
+++ PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNbaBoxscore.cs
@@ -9,7 +9,7 @@ namespace PostSportsToTwitterFunc
     {
         private const MySportsFeedsClient.Sport Sport = MySportsFeedsClient.Sport.NBA;
         private const string TeamAbbreviation = "MIL";
-        private static readonly DateTime ForDate = DateTime.UtcNow - TimeSpan.FromHours(7); // pacific hours so there's time for the game to be finalized
+        private static readonly DateTime ForDate = DateTime.UtcNow - TimeSpan.FromHours(12); // give a longer window for game to be finalized in sports api
 
         [FunctionName("GetNbaBoxscore")]
         public static async Task Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log)
