b507f834b948404e39a1e9810fed1950f94ae904
Hans Wustrack <hans2792@gmail.com>
2019-11-14 07:42:05 +1300

    change nba twitter handle

--- PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNbaBoxscore.cs
+++ PostSportsToTwitterFunc/PostSportsToTwitterFunc/GetNbaBoxscore.cs
@@ -19,7 +19,7 @@ namespace PostSportsToTwitterFunc
                 string gameStatus = await sportsClient.GetGameStatus2Async(Sport, ForDate, TeamAbbreviation);
 
                 TwitterClient twitterClient = new TwitterClient(log);
-                twitterClient.PostTweetIfNotAlreadyPosted("Scores_Bucks", gameStatus);
+                twitterClient.PostTweetIfNotAlreadyPosted("BucksScores", gameStatus);
             }
         }
     }
